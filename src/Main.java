2c949648231bc4c7399009e6a40adc9c1367fc1b
JackStrick <jonstrickland96@gmail.com>
2020-10-22 17:34:48 -0400

    Added user ability to continue or stop program

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,28 @@ public class Main {
 
     public static void main(String[] args){
 
-        String input = JOptionPane.showInputDialog(null, "Enter Your Palindrome: ");
+        String continueTrying;
 
+        do{
+            String input = JOptionPane.showInputDialog(null, "Enter Your Palindrome: ");
 
+            boolean test = Palindrome.isPalindrome(input);
 
-        boolean test = Palindrome.isPalindrome(input);
+            if (test){
+                JOptionPane.showMessageDialog(null, "The phrase you entered is a palindrome");
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "The phrase you entered is not a palindrome ");
+            }
 
-        if (test == true){
-            JOptionPane.showMessageDialog(null, "The phrase you entered is a palindrome");
-        }
-        else{
-            JOptionPane.showMessageDialog(null, "The phrase you entered is not a palindrome ");
-        }
+            continueTrying = JOptionPane.showInputDialog(null, "Would you like to continue? ");
+
+        }while(continueTrying.charAt(0) == 'Y' || continueTrying.charAt(0) == 'y');
+
 
 
         System.exit(0);
-        
+
     }
 
 
